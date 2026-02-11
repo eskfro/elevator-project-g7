4825ea796158c5076beb8a308725342d9ecd6175
mariusersvag <ersvag@hotmail.com>
2026-02-11 21:12:08 +0100

    Takk for meg, (11.02.2026 21:11)

--- internal/ordercontrol/orders.go
+++ internal/ordercontrol/orders.go
@@ -13,6 +13,7 @@ import (
 	"elevator-project-g7/internal/elev"
 	"elevator-project-g7/internal/elevio"
 	"elevator-project-g7/internal/rolemanager"
+	"time"
 )
 
 type OrderStatus int
@@ -52,11 +53,11 @@ func CreateWorldView() WorldView {
 }
 
 func CreateAllWorldViews() [elev.N_MAX_ELEVS]WorldView {
-	var wv [elev.N_MAX_ELEVS]WorldView
+	var allWorldViews [elev.N_MAX_ELEVS]WorldView
 	for i := 0; i < elev.N_MAX_ELEVS; i++ {
-		wv[i] = CreateWorldView()
+		allWorldViews[i] = CreateWorldView()
 	}
-	return wv
+	return allWorldViews
 }
 
 type Order struct {
@@ -66,13 +67,14 @@ type Order struct {
 }
 
 type Channels struct {
-	buttonPress 	  chan elevio.ButtonEvent
-	clearOrder		  chan Order     			//Primary	
-	requestConfirmed  chan Order				//Primary	
-	rcvBcast 		  chan WorldView 			//Primary	
-	newOrderRequest   chan Order				//Primary		
-	msgFromPrimary 	  chan [elev.N_MAX_ELEVS][elev.N_FLOORS][elev.N_BUTTONS]OrderStatus
-	orderBcastTicker  chan struct{}
+	buttonPress      chan elevio.ButtonEvent
+	clearOrder       chan Order     //Primary
+	requestConfirmed chan Order     //Primary
+	rcvBcast         chan WorldView //Primary
+	newOrderRequest  chan Order     //Primary
+	msgFromPrimary   chan [elev.N_MAX_ELEVS][elev.N_FLOORS][elev.N_BUTTONS]OrderStatus
+	roleUpdate       chan rolemanager.Role
+	orderBcastTicker <-chan time.Time
 }
 
 func Start(WorldView *WorldView,
@@ -87,104 +89,108 @@ func Start(WorldView *WorldView,
 			}
 		}
 	}
+
+	ticker := time.NewTicker(100 * time.Millisecond)
+
 	Ch := Channels{
-		buttonPress 		 : make(chan elevio.ButtonEvent),
-		clearOrder		 	 : make(chan Order),       	//Primary
-		requestConfirmed  	 : make(chan Order), 		//Primary
-		rcvBcast 		 	 : make(chan WorldView), 	//Primary		 //TODO: WorldView is not a type
-		newOrderRequest		 : make(chan Order),		//Primary
-		msgFromPrimary 	 	 : make(chan [elev.N_MAX_ELEVS][elev.N_FLOORS][elev.N_BUTTONS]OrderStatus),
-		orderBcastTicker	 : make(chan struct{}),
+		buttonPress:      make(chan elevio.ButtonEvent),
+		clearOrder:       make(chan Order),     //Primary
+		requestConfirmed: make(chan Order),     //Primary
+		rcvBcast:         make(chan WorldView), //Primary		 //TODO: WorldView is not a type
+		newOrderRequest:  make(chan Order),     //Primary
+		msgFromPrimary:   make(chan [elev.N_MAX_ELEVS][elev.N_FLOORS][elev.N_BUTTONS]OrderStatus),
+		roleUpdate:       make(chan rolemanager.Role),
+		orderBcastTicker: ticker.C,
 	}
 
-
 	go elevio.PollButtons(Ch.buttonPress)
+	go rolemanager.PollRoleUpdate(Ch.roleUpdate)
 
-	go orderControl(WorldView, AllWorldViews, Role, &Ch)
+	go orderControl(WorldView, AllWorldViews, Ch)
 }
 
 func orderControl(WorldView *WorldView,
-				  AllWorldViews *[elev.N_MAX_ELEVS]WorldView, //Primary
-			      Role *rolemanager.Role,
-				  Ch *Channels) {
+	AllWorldViews *[elev.N_MAX_ELEVS]WorldView, //Primary
+	Ch Channels) {
+
+	currentRole := rolemanager.ROLE_Backup
 
 	for {
-
-		switch *Role {
-
+		switch currentRole {
 		case rolemanager.ROLE_Backup:
-			for {
-				select {
-				case btnPress := <-Ch.buttonPress:
-					elevio.PrintButtonpress(btnPress)
-					WorldView.OrderTable[rolemanager.Id][btnPress.Floor][btnPress.Button] = OS_REQUESTED
+			//Marius: Fjerna for løkker her inne, slik at det går ann å flytte seg mellom Primary og Backup
+			select {
+			case currentRole = <-Ch.roleUpdate:
 
-				case OrderTableFromPrimary := <-Ch.msgFromPrimary:
-					if true { //not acceptance test passed {//TODO: fix if statement
-						break //TODO: maybe kill???
-					}
+			case btnPress := <-Ch.buttonPress:
+				elevio.PrintButtonpress(btnPress)
+				WorldView.OrderTable[rolemanager.Id][btnPress.Floor][btnPress.Button] = OS_REQUESTED
 
-					WorldView.OrderTable = OrderTableFromPrimary
-
-					for f := 0; f < elev.N_FLOORS; f++ {
-						for b := 0; b < elev.N_BUTTONS; b++ {
-							WorldView.LocalOrderTable[f][b] = OrderTableFromPrimary[rolemanager.Id][f][b] == OS_CONFIRMED
-						}
-					}
-
-				case btnPress := <-Ch.clearOrder:
-					WorldView.OrderTable[rolemanager.Id][btnPress.floor][btnPress.buttonType] = OS_CLEAR
-
-				case <-Ch.orderBcastTicker:
-					//TODO: send heile WorldView til Primary
+			case OrderTableFromPrimary := <-Ch.msgFromPrimary:
+				if true { //not acceptance test passed {//TODO: fix if statement
+					break //TODO: maybe kill???
 				}
+
+				WorldView.OrderTable = OrderTableFromPrimary
+
+				for f := 0; f < elev.N_FLOORS; f++ {
+					for b := 0; b < elev.N_BUTTONS; b++ {
+						WorldView.LocalOrderTable[f][b] = OrderTableFromPrimary[rolemanager.Id][f][b] == OS_CONFIRMED
+					}
+				}
+
+			case btnPress := <-Ch.clearOrder:
+				WorldView.OrderTable[rolemanager.Id][btnPress.floor][btnPress.buttonType] = OS_CLEAR
+
+			case <-Ch.orderBcastTicker:
+				//TODO: send heile WorldView til Primary
 			}
 
 		case rolemanager.ROLE_Primary:
-			for {
 
-				select {//TODO: Slik det er no vil goroutinen blokke fram til noken leser (f.eks. Ch.clearOrder <-order) 
-						//		Ingen vil lese fordi det skjer i samme routine.
-						//		Alternativer: - Kjør logikk direkte								⭐⭐⭐
-						//					  - Lag funksjoner som kalles direkte				⭐⭐⭐
-						//					  - Behold channels men i to separate go-routines	🤨🏎️
-						//					  - Behold channels men med buffer					🙅
+			select { //TODO: Slik det er no vil goroutinen blokke fram til noken leser (f.eks. Ch.clearOrder <-order)
+			//		Ingen vil lese fordi det skjer i samme routine.
+			//		Alternativer: - Kjør logikk direkte								⭐⭐⭐
+			//					  - Lag funksjoner som kalles direkte				⭐⭐⭐
+			//					  - Behold channels men i to separate go-routines	🤨🏎️
+			//					  - Behold channels men med buffer					🙅
 
-				case order := <-Ch.newOrderRequest:
-					//TODO: IdForOrder := Calculate which elevator
-					WorldView.OrderTable[IdForOrder][order.floor][order.buttonType] = OS_REQUESTED
+			case currentRole = <-Ch.roleUpdate:
 
-				case order := <-Ch.requestConfirmed:
-					WorldView.OrderTable[order.elevatorNumber][order.floor][order.buttonType] = OS_CONFIRMED
+			case order := <-Ch.newOrderRequest:
+				//TODO: IdForOrder := Calculate which elevator
+				WorldView.OrderTable[IdForOrder][order.floor][order.buttonType] = OS_REQUESTED
 
-				case order := <-Ch.clearOrder:
-					//TODO: Acceptance test
-					WorldView.OrderTable[order.elevatorNumber][order.floor][order.buttonType] = OS_NO_ORDER
-				
-				case rcvWorldView := <-Ch.rcvBcast: //TODO: Sende kun OrderTable og ikkje heile worldview???
-					// TODO: Update tables based on changes made
+			case order := <-Ch.requestConfirmed:
+				WorldView.OrderTable[order.elevatorNumber][order.floor][order.buttonType] = OS_CONFIRMED
 
-					for n_e := 0; n_e < rolemanager.NumElevs; n_e++ {
-						for f := 0; f < elev.N_FLOORS; f++ {
-							for b := 0; b < elev.N_BUTTONS; b++ {
+			case order := <-Ch.clearOrder:
+				//TODO: Acceptance test
+				WorldView.OrderTable[order.elevatorNumber][order.floor][order.buttonType] = OS_NO_ORDER
 
-								if rcvWorldView.OrderTable[n_e][f][b] == WorldView.OrderTable[n_e][f][b]{
-									continue
-								}
+			case rcvWorldView := <-Ch.rcvBcast: //TODO: Sende kun OrderTable og ikkje heile worldview???
+				// TODO: Update tables based on changes made
 
-								order := Order{
-									floor:          f,
-									elevatorNumber: n_e,
-									buttonType:     elevio.ButtonType(b),
-								}
+				for n_e := 0; n_e < rolemanager.NumElevs; n_e++ {
+					for f := 0; f < elev.N_FLOORS; f++ {
+						for b := 0; b < elev.N_BUTTONS; b++ {
 
-								if rcvWorldView.OrderTable[n_e][f][b] == OS_CLEAR {
-									Ch.clearOrder <- order
-								}
+							if rcvWorldView.OrderTable[n_e][f][b] == WorldView.OrderTable[n_e][f][b] {
+								continue
+							}
 
-								if isConfirmedByAll(AllWorldViews, order) {
-									Ch.requestConfirmed <- order
-								}
+							order := Order{
+								floor:          f,
+								elevatorNumber: n_e,
+								buttonType:     elevio.ButtonType(b),
+							}
+
+							if rcvWorldView.OrderTable[n_e][f][b] == OS_CLEAR {
+								Ch.clearOrder <- order
+							}
+
+							if isConfirmedByAll(AllWorldViews, order) {
+								Ch.requestConfirmed <- order
 							}
 						}
 					}
--- internal/rolemanager/rolemanager.go
+++ internal/rolemanager/rolemanager.go
@@ -1,5 +1,9 @@
 package rolemanager
 
+import "time"
+
+const POLL_RATE = 1 * time.Second
+
 /*
 - ElevatorRole
 - AliveList
@@ -11,5 +15,19 @@ type Role int
 const (
 	ROLE_Backup  Role = 0
 	ROLE_Primary Role = 1
+	ROLE_Init	 Role = 2
 )
+
 //TODO: define NumElevs, Id
+
+func PollRoleUpdate(receiver chan<- Role) {
+	prev := ROLE_Init
+	for {
+		time.Sleep(POLL_RATE)
+		current := //TODO: Nåverande rolle
+		if current != prev && v != ROLE_Init {
+			receiver <- current
+		}
+		prev = current
+	}
+}
