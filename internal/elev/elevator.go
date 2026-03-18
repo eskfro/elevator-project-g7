1b439a1f380b811d9c8d85e516728a94a1dd2934
eskfro <eskfro@gmail.com>
2026-03-19 00:51:58 +0100

    endra timer til 10

--- internal/elev/elevator.go
+++ internal/elev/elevator.go
@@ -15,7 +15,7 @@ const PRIMARY_ELECTION_DELAY = 401 * time.Millisecond
 
 const ORDER_TIMEOUT = 20 * time.Second
 const STUCK_DOOR_TIMEOUT = 15 * time.Second
-const IDLE_RESTART_TIMEOUT = 1000 * time.Second
+const IDLE_RESTART_TIMEOUT = 10 * time.Second
 const HEARTBEAT_TIMEOUT = 701 * time.Millisecond
 const BETWEEN_FLOORS_TIMEOUT = 7 * time.Second
 
