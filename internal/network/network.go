b46e3c8fc2ee44d2d68302be8da819263ffd8923
eskfro <eskfro@gmail.com>
2026-03-18 22:19:17 +0100

    Network og pp enable

--- internal/network/network.go
+++ internal/network/network.go
@@ -9,8 +9,8 @@ import (
 )
 
 // Network Config
-const BCAST_INTERVAL_HB = 30 * time.Millisecond //HeartBeats
-const BCAST_INTERVAL_OT = 30 * time.Millisecond //OrderTablePackets
+const BCAST_INTERVAL_HB = 35 * time.Millisecond //HeartBeats
+const BCAST_INTERVAL_OT = 35 * time.Millisecond //OrderTablePackets
 const BCAST_RCV_IP = "255.255.255.255"
 
 type Ports struct {
