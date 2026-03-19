22f0f3a0897c16288e7949030d1a53ab7118bde7
eskfro <eskfro@gmail.com>
2026-03-19 02:02:46 +0100

    60s idle timer

--- internal/elev/elevator.go
+++ internal/elev/elevator.go
@@ -15,10 +15,10 @@ const PRIMARY_ELECTION_DELAY = 401 * time.Millisecond
 
 const ORDER_TIMEOUT = 20 * time.Second
 const STUCK_DOOR_TIMEOUT = 15 * time.Second
-const IDLE_RESTART_TIMEOUT = 1000 * time.Second
+const IDLE_RESTART_TIMEOUT = 60 * time.Second
 const HEARTBEAT_TIMEOUT = 701 * time.Millisecond
 const BETWEEN_FLOORS_TIMEOUT = 7 * time.Second
-																		
+
 const RECONNECT_SYNC_WINDOW = 1000 * time.Millisecond
 
 const INVALID_PRIMARY_ID = N_MAX_ELEVS + 1
