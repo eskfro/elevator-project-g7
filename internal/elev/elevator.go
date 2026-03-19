d18f27452f2083464d99a03e139b5258300dd1a7
eskfro <eskfro@gmail.com>
2026-03-19 01:02:21 +0100

    Timer 1000

--- internal/elev/elevator.go
+++ internal/elev/elevator.go
@@ -15,7 +15,7 @@ const PRIMARY_ELECTION_DELAY = 401 * time.Millisecond
 
 const ORDER_TIMEOUT = 20 * time.Second
 const STUCK_DOOR_TIMEOUT = 15 * time.Second
-const IDLE_RESTART_TIMEOUT = 10 * time.Second
+const IDLE_RESTART_TIMEOUT = 1000 * time.Second
 const HEARTBEAT_TIMEOUT = 701 * time.Millisecond
 const BETWEEN_FLOORS_TIMEOUT = 7 * time.Second
 
@@ -86,7 +86,7 @@ type ElevatorPhysicalInfo struct {
 
 type Elevator struct {
 	PhysicalInfo   ElevatorPhysicalInfo
-	OrderTable     OrderTable // Table of all the orders (floor, btn) for all elevators
+	OrderTable     OrderTable     // Table of all the orders (floor, btn) for all elevators
 	AllOrderTables AllOrderTables // Primaries overview of everyones OrderTable
 	AliveList      AliveList
 	NumElevs       int
